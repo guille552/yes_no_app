3d3e6b4150efd7a62106197ca86efb508d52e83c
guille552 <guillermo.medina021102@gmail.com>
2024-05-02 07:29:00 -0600

    modificaciones 02/05/24

--- lib/config/helpers/get_yes_o_no_answer.dart
+++ lib/config/helpers/get_yes_o_no_answer.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:http/http.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
+
+
 class GetYesNoAnswer{
   final _dio = Dio();
   Future<Message> getAnswer() async{
